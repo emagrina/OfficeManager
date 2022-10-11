29f130f956e81894e276e25daab3818a10f8d508
Paunetum <pau.garcia.ext@gmali.com>
2022-10-11 12:48:47 +0200

    data access

---
+++ OfficeManagerAPI/OfficeManagerAPI/DataAccess/OfficeDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfficeManagerAPI.Models.DataModels;
+
+namespace OfficeManagerAPI.DBAccess
+{
+    public class OfficeDBContext : DbContext
+    {
+        public OfficeDBContext(DbContextOptions<OfficeDBContext> options) : base(options)
+        {
+
+        }
+
+        // Tables of the DataBase
+        public DbSet<Chair>? Chairs { get; set; }
+        public DbSet<User>? Users { get; set; }
+    }
+}
