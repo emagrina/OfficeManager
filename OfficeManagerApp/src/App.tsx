7159688af52be09e3fd15a4093e7f2445880b96c
Ernest Magriñá <ernest.magrina.ext@inetum.com>
2022-10-15 03:44:44 +0200

    feat: Prueba de conexion a la API con Axios

--- OfficeManagerApp/src/App.tsx
+++ OfficeManagerApp/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios, {AxiosResponse} from 'axios';
+import React, {useEffect} from 'react';
+import {urlClimas} from './endpoints';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   useEffect(() =>{
+       axios.get(urlClimas)
+           .then((response: AxiosResponse<any>) => {
+               console.log(response.data);
+           })
+   }, [])
+    return (
+        <div>
+            <h1>hello!</h1>
+        </div>
+    );
 }
 
 export default App;
