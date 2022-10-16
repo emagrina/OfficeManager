26eecde4752a484704cccd6e0683955a2f3853d4
Ernest Magriñá <ernest8081@gmail.com>
2022-10-16 19:01:53 +0200

    refactor: Actualizado los Cores

--- OfficeManagerApp/src/App.tsx
+++ OfficeManagerApp/src/App.tsx
@@ -1,19 +1,18 @@
-import axios, {AxiosResponse} from 'axios';
-import React, {useEffect} from 'react';
-import {urlClimas} from './endpoints';
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect } from 'react';
+import { urlClimas } from './endpoints';
 
 function App() {
-   useEffect(() =>{
-       axios.get(urlClimas)
-           .then((response: AxiosResponse<any>) => {
-               console.log(response.data);
-           })
-   }, [])
-    return (
-        <div>
-            <h1>hello!</h1>
-        </div>
-    );
+	useEffect(() => {
+		axios.get(urlClimas).then((response: AxiosResponse<any>) => {
+			console.log(response.data);
+		});
+	}, []);
+	return (
+		<div>
+			<h1>hello!</h1>
+		</div>
+	);
 }
 
 export default App;
