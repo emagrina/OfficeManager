9f61bcdef46e0bef621e87f7b3ea92ba5c0e456f
Ernest Magriñá <ernest8081@gmail.com>
2022-10-17 01:23:54 +0200

    refactor: Modificacion al iniciar Eslint y Prettier

--- OfficeManagerApp/src/main.tsx
+++ OfficeManagerApp/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
-import "./index.css";
+import './index.css';
 
 const app: any = <h1>Hello!</h1>;
-const container: any = document.getElementById("root")
+const container: any = document.getElementById('root');
 
 ReactDOM.render(app, container);
