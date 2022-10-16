ddf4fb4b3eb121c2b78381aa19590eed582954a2
Ernest Magriñá <ernest8081@gmail.com>
2022-10-17 00:31:36 +0200

    feat: Agregado la parte Frontend con Vite

---
+++ OfficeManagerApp/src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+const app: any = <h1>Hello!</h1>;
+const container: any = document.getElementById("root")
+
+ReactDOM.render(app, container);
