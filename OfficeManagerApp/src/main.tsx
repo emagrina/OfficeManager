d4f8b951838ae71a6a376535401614efb2987c2d
Ernest Magriñá <ernest8081@gmail.com>
2022-10-17 03:25:12 +0200

    feat: Agregado el componente App

--- OfficeManagerApp/src/main.tsx
+++ OfficeManagerApp/src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 
 const app: any = <h1>Hello!</h1>;
 const container: any = document.getElementById('root');
 
-ReactDOM.render(app, container);
+ReactDOM.render(<App />, container);
