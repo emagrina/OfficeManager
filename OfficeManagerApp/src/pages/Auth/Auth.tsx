7ec019e7e491f482c63732b8d3c9d2c715a7dcb3
LLUIS ISERN LOPEZ <lluis.isern.ext@iecisa.corp>
2022-10-20 11:29:50 +0200

    style: Estilos AuthForm

--- OfficeManagerApp/src/pages/Auth/Auth.tsx
+++ OfficeManagerApp/src/pages/Auth/Auth.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import AuthForm from './components/AuthForm/AuthForm';
 
 export default function LoginUser() {
-	return <div>LoginUser</div>;
+	return (
+		<>
+		<AuthForm/>
+		</>
+		
+	);
 }
---
+++ OfficeManagerApp/src/pages/Auth/components/AuthForm/AuthForm.tsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import '../../../../styles/sass/main.scss';
+
+const AuthForm = () => {
+    return (
+        <>
+        <div className="container">
+            <h1>¡Bienvenido/a!</h1>
+            <form>
+                <label htmlFor="email">Email</label><br/>
+                <input type="email" id="email" name="email"/><br/><br/>
+                <label htmlFor="password">Contraseña</label><br/>
+                <input type="password" id="password" name="password"/><br/>
+                <div className="auth-error"> ERROR: Usuario o contraseña incorrectos </div>
+                <input type="checkbox" id="remember" name="remember"/>
+                <label htmlFor="remember">Recuérdame</label>
+                <a href="#">¿Olvidaste la contraseña?</a><br/>
+                <input type="submit" value="Iniciar sesión"/>
+            </form>
+        </div>
+        </>
+    );
+};
+
+export default AuthForm;
