faa81c973b1373f076de99a1ab18ecb09bf057bb
Judit Sansó López <judit.sanso.ext@inetum.com>
2022-10-21 09:59:57 +0200

    a

--- OfficeManagerApp/src/pages/Admin/Admin.tsx
+++ OfficeManagerApp/src/pages/Admin/Admin.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import SideBar from './components/SideBar';
+import Header from '../../components/Header/Header';
+import '../../styles/sass/layout/__layouts-dir.scss'
 
 export default function Admin() {
-	return <div>Admin</div>;
-}
+	return( 
+	<div className='admin'>
+		{/* <Header/> */}
+		<SideBar></SideBar>
+	</div>
+	);
+};
---
+++ OfficeManagerApp/src/pages/Admin/components/SideBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SideBar = () => {
+	return (
+		<div className='sideBar'>
+			<h3>Panel de administrador</h3>
+
+      <ul>
+        <li> Gestor de usuarios </li>
+        <li> Gestor de salas </li>
+        <li> Gestor de reservas </li>
+      </ul>
+
+		</div>
+	);
+};
+
+export default SideBar;
