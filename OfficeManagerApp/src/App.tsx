a5465d044a2afbf51c5c3b92481add7d6330bf62
Ernest Magriñá <ernest8081@gmail.com>
2022-10-23 07:55:10 +0200

    refacor: Mejora de la app

--- OfficeManagerApp/src/App.tsx
+++ OfficeManagerApp/src/App.tsx
@@ -1,3 +1,15 @@
-const App = () => <h1>Hello!</h1>;
+// @ts-ignore
+import * as eva from 'eva-icons';
+import { Auth } from './pages';
+import { useEffect } from 'react';
+import './styles/sass/main.scss';
+
+const App = () => {
+	useEffect(() => {
+		// add this line
+		eva.replace();
+	}, []);
+	return <Auth />;
+};
 
 export default App;
--- OfficeManagerApp/src/main.tsx
+++ OfficeManagerApp/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Auth } from './pages';
+import { Home } from './pages';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<Auth />
+	<App />
 );
