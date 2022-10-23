18d8ffbfa685d096f84ab19249915ef9d300289d
Ernest Magriñá <ernest8081@gmail.com>
2022-10-24 01:44:11 +0200

    feat: Agregadas las rutas de la aplicación

--- OfficeManagerApp/src/App.tsx
+++ OfficeManagerApp/src/App.tsx
@@ -1,15 +1,31 @@
-// @ts-ignore
-import * as eva from 'eva-icons';
-import { Auth } from './pages';
+import {Admin, Auth, Home} from './pages';
 import { useEffect } from 'react';
 import './styles/sass/main.scss';
+// @ts-ignore
+import * as eva from 'eva-icons';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from "react-router-dom";
 
 const App = () => {
 	useEffect(() => {
 		// add this line
 		eva.replace();
 	}, []);
-	return <Auth />;
+	
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Auth />}/>
+				<Route path="/home" element={<Home />}/>
+				<Route path="/home" element={<Admin />}/>
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
