bb610778f254b8f6253478652e008d6a99c415bb
LLUIS ISERN LOPEZ <lluis.isern.ext@iecisa.corp>
2022-10-24 12:58:25 +0200

    style: Imagen del login y estilos + arreglar estilos botones

--- OfficeManagerApp/src/pages/Auth/Auth.tsx
+++ OfficeManagerApp/src/pages/Auth/Auth.tsx
@@ -4,7 +4,6 @@ import AuthForm from './components/AuthForm/AuthForm';
 function Auth() {
 	return (
 		<div className='authBody'>
-			<div className='authImage'>Imagen</div>
 			<div className='authForm'>
 				<AuthForm />
 			</div>
