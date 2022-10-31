a3f6bfa24dd93685bf2457e6f96dfc10f620dad9
LLUIS ISERN LOPEZ <lluis.isern.ext@iecisa.corp>
2022-10-31 08:29:56 +0100

    style: Pagina profile

--- OfficeManagerApp/src/pages/Profile/Profile.tsx
+++ OfficeManagerApp/src/pages/Profile/Profile.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
+// @ts-ignore
+import Imagen from '../../assets/img/RandomPerson.jpg';
 
-function Profile() {
+function Profile({ name, lastName }: any) {
 	return (
 		<div className='profileBody'>
-			<section className='profileHeader'>Hola usuario</section>
+			<section className='profileHeader'>
+				<img src={Imagen} className='profileImage' /> <span className='profileName'> Lluís Isern </span>
+			</section>
 			<section className='profileLinks'>
-				<NavLink to={'./data'}>Perfil</NavLink>
+				<NavLink 
+					to={'./data'}
+					className={({ isActive }) =>
+						isActive ? 'activeStyle' : 'inactiveStyle'
+					}
+				>
+					Perfil
+				</NavLink>
 				<br />
-				<NavLink to={'./account'}>Cuenta</NavLink>
+				<br />
+				<NavLink 
+					to={'./account'}
+					className={({ isActive }) =>
+						isActive ? 'activeStyle' : 'inactiveStyle'
+					}
+				>
+					Cuenta
+				</NavLink>
 			</section>
 			<section className='profileContent'>
 				<Outlet />
