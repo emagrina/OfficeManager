d451e1d2e1b8383c98a0f86363d96ab34c576b72
Paunetum <pau.garcia.ext@gmali.com>
2022-11-04 12:53:29 +0100

    feat: changes on booking controller, chairs and rooms  availability

--- OfficeManagerAPI/OfficeManagerAPI/Migrations/20221104114108_nullable field in rooms.Designer.cs
+++ OfficeManagerAPI/OfficeManagerAPI/Migrations/20221104114108_nullable field in rooms.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OfficeManagerAPI.DBAccess;
 
@@ -11,9 +12,10 @@ using OfficeManagerAPI.DBAccess;
 namespace OfficeManagerAPI.Migrations
 {
     [DbContext(typeof(OfficeDBContext))]
-    partial class OfficeDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221104114108_nullable field in rooms")]
+    partial class nullablefieldinrooms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -70,9 +72,6 @@ namespace OfficeManagerAPI.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
-                    b.Property<bool>("Available")
-                        .HasColumnType("bit");
-
                     b.Property<string>("Position")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
@@ -89,9 +88,6 @@ namespace OfficeManagerAPI.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                    b.Property<bool>("Available")
-                        .HasColumnType("bit");
 
                     b.Property<string>("Name")
                         .IsRequired()
--- OfficeManagerAPI/OfficeManagerAPI/Models/DataModels/Booking.cs
+++ OfficeManagerAPI/OfficeManagerAPI/Models/DataModels/Booking.cs
@@ -9,14 +9,14 @@ namespace OfficeManagerAPI.Models.DataModels
         public int Id { get; set; }
         [Required]
         public DateTime DateTime { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
 
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
 
 
-        public virtual Chair Chair { get; set; }
-        public virtual Room Room { get; set; }
+        public virtual Chair? Chair { get; set; }
+        public virtual Room? Room { get; set; }
         public virtual User User { get; set; }
     }
 }
