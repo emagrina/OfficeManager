bb07f8ba6a89a8dc3ef762cb751010dcae1f6f05
LLUIS ISERN LOPEZ <lluis.isern.ext@iecisa.corp>
2022-11-04 08:54:19 +0100

    fix: Rooms/Booking imports

--- OfficeManagerApp/src/pages/Admin/components/BookingsManagment.tsx
+++ OfficeManagerApp/src/pages/Admin/components/BookingsManagment.tsx
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const BookingsManagment = () => {
-	return (
-		<div className='bookingsManagment'>
-			<h2> Gestor de reservas </h2>
-		</div>
-	);
+	const url = "https://localhost:7016/api/Bookings";
+	const bookingsStart = [
+		{ID: 1, Description: ''},
+		{ID: 1, Description: ''}
+	]
+	const [bookings, setBookings] = useState(bookingsStart);
+	const [hasLoaded, setHasLoaded] = useState('noLoaded');
+
+	useEffect(() => {
+		getBookings();
+	  }, [])
+
+	const getBookings = async () => {
+		await axios
+			.get(url, {
+				headers: {
+					'Access-Control-Allow-Origin': '*'
+				},
+			})
+			.then(response => {
+				console.log(response.data);		
+				
+				let dbBookings = [];
+				for (const booking of response.data) {
+					dbBookings.push({ID: booking.id, Description: booking.description})
+					
+				}
+				setBookings(dbBookings);
+				setHasLoaded('loaded')
+			})
+			.catch(error => {
+				console.log(error);
+				setHasLoaded('error')
+			});
+	};
+
+	if(hasLoaded == 'loaded'){
+		if(bookings.length === 0){
+			return (
+				<div className='bookingManagment'>
+					<h2> Gestor de reservas </h2>
+					<div>
+						No hay reservas.
+					</div>
+				</div>
+			);
+		} else{
+			return (
+				<div className='bookingManagment'>
+					<h2> Gestor de reservas </h2>
+					<div className='table'>
+						<table>
+							<tbody>
+								<tr>
+									{Object.keys(bookings[0]).map((key) => (
+										<th className={key}>{key}</th>
+									))}
+								</tr>
+								{bookings.map((item) => (
+									<tr key={item.ID}>
+									{Object.values(item).map((val) => (
+										(val == 'edit')? <td> <button> edit </button> </td> :
+										(val == 'delete')? <td> <button> delete </button> </td> :
+										<td>{val}</td>
+									))}
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			);
+		}
+	}else if(hasLoaded == 'noLoaded'){
+		return (
+			<div className='bookingManagment'>
+				Cargando ...
+			</div>
+		);
+	}else if(hasLoaded == 'error'){
+		return (
+			<div className='bookingManagment'>
+				Error
+			</div>
+		);
+	}
 };
 
 export default BookingsManagment;
--- OfficeManagerApp/src/pages/Admin/components/RoomsManagment.tsx
+++ OfficeManagerApp/src/pages/Admin/components/RoomsManagment.tsx
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const RoomnsManagment = () => {
-	return (
-		<div className='roomsManagment'>
-			<h2> Gestor de salas </h2>
-		</div>
-	);
+const RoomsManagment = () => {
+	const url = "https://localhost:7016/api/Rooms";
+	const roomsStart = [
+		{ID: 1, Name: '',},
+		{ID: 1, Name: '',}
+	]
+	const [rooms, setRooms] = useState(roomsStart);
+	const [hasLoaded, setHasLoaded] = useState('noLoaded');
+
+	useEffect(() => {
+		getRooms();
+	  }, [])
+
+	const getRooms = async () => {
+		await axios
+			.get(url, {
+				headers: {
+					'Access-Control-Allow-Origin': '*'
+				},
+			})
+			.then(response => {
+				console.log(response.data);		
+				
+				let dbRooms = [];
+				for (const room of response.data) {
+					dbRooms.push({ID: room.id, Name: room.name})
+					
+				}
+				setRooms(dbRooms);
+				setHasLoaded('loaded')
+			})
+			.catch(error => {
+				console.log(error);
+				setHasLoaded('error')
+			});
+	};
+
+	if(hasLoaded == 'loaded'){
+		if(rooms.length === 0){
+			return (
+				<div className='roomManagment'>
+					<h2> Gestor de salas </h2>
+					<div>
+						No hay salas.
+					</div>
+				</div>
+			);
+		} else{
+			return (
+				<div className='roomManagment'>
+					<h2> Gestor de salas </h2>
+					<div className='table'>
+						<table>
+							<tbody>
+								<tr>
+									{Object.keys(rooms[0]).map((key) => (
+										<th className={key}>{key}</th>
+									))}
+								</tr>
+								{rooms.map((item) => (
+									<tr key={item.ID}>
+									{Object.values(item).map((val) => (
+										(val == 'edit')? <td> <button> edit </button> </td> :
+										(val == 'delete')? <td> <button> delete </button> </td> :
+										<td>{val}</td>
+									))}
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			);
+		}
+	}else if(hasLoaded == 'noLoaded'){
+		return (
+			<div className='roomManagment'>
+				Cargando ...
+			</div>
+		);
+	}else if(hasLoaded == 'error'){
+		return (
+			<div className='roomManagment'>
+				Error
+			</div>
+		);
+	}
 };
 
-export default RoomnsManagment;
+export default RoomsManagment;
