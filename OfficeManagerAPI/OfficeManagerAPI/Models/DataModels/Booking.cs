cf22c1f750e007bbbf91ac418adf0d24dd087557
Paunetum <pau.garcia.ext@gmali.com>
2022-11-11 12:37:32 +0100

    feat: fluent api

--- OfficeManagerAPI/OfficeManagerAPI/Models/DataModels/Booking.cs
+++ OfficeManagerAPI/OfficeManagerAPI/Models/DataModels/Booking.cs
@@ -7,11 +7,19 @@ namespace OfficeManagerAPI.Models.DataModels
         [Required]
         [Key]
         public int Id { get; set; }
+
         [Required]
+        /*[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]*/
         public DateTime DateTime { get; set; }
         public string? Description { get; set; } = string.Empty;
 
+        /*[DisplayFormat(DataFormatString = "{0:hh:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Time)]*/
         public DateTime? StartTime { get; set; }
+
+        /*[DisplayFormat(DataFormatString = "{0:hh:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Time)]*/
         public DateTime? EndTime { get; set; }
 
 
