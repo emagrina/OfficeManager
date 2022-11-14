237ad9f007014561599483a4db5d1b16848ac5fd
Paunetum <114986355+Paunetum@users.noreply.github.com>
2022-11-14 13:19:19 +0100

    feat: fluent api v1

--- OfficeManagerAPI/OfficeManagerAPI/DataAccess/OfficeDBContext.cs
+++ OfficeManagerAPI/OfficeManagerAPI/DataAccess/OfficeDBContext.cs
@@ -21,11 +21,61 @@ namespace OfficeManagerAPI.DBAccess
             base.OnModelCreating(modelBuilder);
             // modelBuilder.Seed();
 
-            foreach ( var foreignKey in modelBuilder.Model.GetEntityTypes()
+            /*foreach ( var foreignKey in modelBuilder.Model.GetEntityTypes()
                 .SelectMany(e => e.GetForeignKeys()))
             {
                 foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
-            }
+            }*/
+
+            modelBuilder.Entity<Chair>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Position).IsRequired();
+
+                entity.Property(x => x.Available).IsRequired();
+            });
+
+            modelBuilder.Entity<Room>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(entity => entity.Size).IsRequired();
+
+                entity.Property(entity => entity.Position).IsRequired();
+
+                entity.Property(entity => entity.Available).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Id).ValueGeneratedOnAdd();
+
+                entity.Property(x => x.FirstName).IsRequired().HasMaxLength(50);
+
+                entity.Property(x => x.LastName).IsRequired().HasMaxLength(50);
+
+                entity.Property(x => x.IsAdmin).IsRequired().HasDefaultValue(false);
+
+                entity.Property(x => x.Email).IsRequired();
+
+                entity.Property(x => x.Passw).IsRequired();
+            });
+
+            modelBuilder.Entity<Booking>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Id).ValueGeneratedOnAdd();
+
+                entity.Property(x => x.DateTime).IsRequired();
+
+                entity.Property(x => x.StartTime);
+
+                entity.Property(x => x.EndTime);
+            });
         }
     }
 }
