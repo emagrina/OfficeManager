3a57a2cbce02b13ef147218a05f6e86146cd2514
emagrina <ernest8081@gmail.com>
2022-11-29 11:59:12 +0100

    refactor: push

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,3 +13,7 @@ docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker build ./FrontOffice -t $DOCKER_USER/ReactOffice:latest
 
 docker build ./nginx -t $DOCKER_USER/NginxOffice:latest
+
+docker push $DOCKER_USER/ReactOffice:latest
+
+docker push $DOCKER_USER/NginxOffice:latest
