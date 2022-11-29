402204dbff4a96000f1245a1bd614d9fb00d32cb
emagrina <ernest8081@gmail.com>
2022-11-29 11:35:58 +0100

    feat: Dockerfile

---
+++ deploy/deploy.sh
@@ -0,0 +1,15 @@
+﻿dotenv () {
+  set -a
+  [ -f .env ] && . .env
+  set +a
+}
+
+dotenv
+
+# UPLOAD IMAGES TO DOCKER HUB
+
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+
+docker build ./FrontOffice -t $DOCKER_USER/ReactOffice:latest
+
+docker build ./nginx -t $DOCKER_USER/NginxOffice:latest
