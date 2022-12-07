16d4e664a5a25f5afda00537655f58fb17cb2aff
Ernest Magriñá <ernest8081@gmail.com>
2022-12-07 01:23:14 +0100

    refactor: pruebas

--- FrontOffice/vite.config.ts
+++ FrontOffice/vite.config.ts
@@ -1,34 +1,28 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
+
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@assets': path.resolve(__dirname, './src/assets'),
-			'@components': path.resolve(__dirname, './src/components'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@services': path.resolve(__dirname, './src/services'),
-			'@utils': path.resolve(__dirname, './src/utils'),
-		},
-	},
-	plugins: [
-		react(),
-		viteCompression({
-			verbose: true,
-			algorithm: 'gzip',
-			ext: '.gz',
-			deleteOriginFile: true,
-		}),
-	],
-	server: {
-		watch: {
-			usePolling: true,
-		},
-		host: true,
-		strictPort: false,
-		port: 4000, // you can replace this port with any port
-	},
+    resolve: {
+        alias: {
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@services': path.resolve(__dirname, './src/services'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+        },
+    },
+    plugins: [
+        react(),
+    ],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true,
+        strictPort: false,
+        port: 4000, // you can replace this port with any port
+    },
 });
