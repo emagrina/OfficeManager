473bd707a390842f1a7b0e421c7205bd259c4666
emagrina <ernest8081@gmail.com>
2022-12-19 12:32:25 +0100

    a

--- buildAll.sh
+++ buildAll.sh
@@ -2,6 +2,8 @@
 
 echo "--------------------[ Docker ]------------------------"
 
-docker-compose up
+docker-compose up -d
+# ERROR: strconv.Atoi: parsing "": invalid syntax
+# SOLUTION: docker-compose down --remove-orphans
 docker ps
-docker images -a
+# docker exec -it officemanager-dev-1 sh
